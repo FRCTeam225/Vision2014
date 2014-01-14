e26a185c3b960cf5d3857201784d9d5e7feb8e95
Andrew <andrew@lobos.me>
2014-01-13 19:43:27 -0500

    First commit

---
+++ TargetTracker.js
@@ -0,0 +1,136 @@
+var cv = require("opencv"),
+        http = require("http"),
+        express = require("express"),
+        vapix = require('vapix'),
+        table = require('./networktable'),
+        app = express();
+
+table.connect("10.2.25.2");
+
+var stream_options = {
+    resolution: '640x480',
+    compression: 30,
+    fps: 5
+}
+
+var camera = vapix.createCamera({
+  address: '10.2.25.11',
+  port: '80',
+  username: 'root',
+  password: 'frc225'
+});
+
+var image_stream = camera.createVideoStream(stream_options);
+
+var currentFrame = undefined;
+var target_data = {};
+
+var high_thresh = [120,255,255];
+var low_thresh = [55,90,20];
+
+
+image_stream.on("data", function(imData)
+{
+        cv.readImage(imData, function(err, im)
+        {
+                imCopy = im.copy();
+                im.convertHSVscale();
+                im.inRange(low_thresh, high_thresh);
+                im.erode(2);
+                im.dilate(3);
+
+                var hasTarget = false;
+                contours = im.findContours();
+                for ( var i = 0; i < contours.size(); i++ )
+                {
+                        contours.approxPolyDP(i, 7, true);
+                        rect = contours.boundingRect(i);
+                        rect.ratio = rect.width/rect.height;
+                        //console.log(rect);
+                        if ( contours.cornerCount(i) == 4  && rect.ratio > 2 )
+                        {
+                                hasTarget = true;
+                                target_data = rect;
+                                console.log(rect);
+                                imCopy.drawContour(contours, i, [255,0,0]);
+                                break;
+                        }
+                        else
+                        {
+                                target_data = {};
+                        imCopy.drawContour(contours, i, [0,0,255]);
+                        }
+                }
+                table.set("/test/hasTarget", hasTarget);
+                currentFrame = imCopy;
+        });
+});
+
+app.get("/target.mjpeg", function(req,res)
+{
+  if ( currentFrame == undefined )
+  {
+    res.send("no image");
+  }
+  else
+  {
+        res.writeHead(200, {
+            'Content-Type': 'multipart/x-mixed-replace; boundary=first',
+           'Cache-Control': 'no-cache',
+           'Connection': 'close',
+           'Pragma': 'no-cache'
+        });
+
+//      res.write(currentFrame.toBuffer(), "binary");
+//      res.end();
+
+        setInterval(function(res)
+        {
+//              console.log("called!");
+//              console.log(res);
+                res.write("--first\r\n");
+
+                res.write("Content-type: image/jpeg\r\n");
+                res.write("Content-Length: "+currentFrame.toBuffer().length+"\r\n");
+                res.write("\r\n");
+
+                res.write(currentFrame.toBuffer(), "binary");
+                res.write("\r\n");
+//              if ( !res )
+//                      res.end();
+        }, 200,res);
+  }
+});
+
+app.get("/target.jpg", function(req,res)
+{
+  if ( currentFrame == undefined )
+  {
+    res.send("no image");
+  }
+  else
+  {
+        res.writeHead(200, {
+           'Cache-Control': 'no-cache',
+           'Connection': 'close',
+           'Pragma': 'no-cache'
+        });
+
+        res.write(currentFrame.toBuffer(), "binary");
+        res.end();
+  }
+});
+
+
+app.get("/target.json", function(req,res)
+{
+        res.json(target_data);
+});
+
+app.get("/", function(req,res)
+{
+        res.send("<img src=\"/target.mjpeg\"></img>");
+});
+
+app.listen(8081);
+
---
+++ networktable.js
@@ -0,0 +1,130 @@
+var Dissolve = require("dissolve"),
+        Concentrate = require("concentrate"),
+    net = require("net");
+
+var entries = new Array();
+
+function packAssignmentValue(stack, value)
+{
+        if ( typeof value == "boolean" )
+                return stack.uint8(value?0x01:0x00);
+        else if ( typeof value == "number" )
+                return stack.doublebe(value);
+        return null;
+}
+
+function getAssignmentValueType(value)
+{
+        if ( typeof value == "boolean" )
+                return 0x00;
+        else if ( typeof value == "number" )
+                return 0x01;
+        return 0x01;
+}
+
+
+var parser = Dissolve().loop(function()
+{
+        this.uint8("msgType").tap(function()
+        {
+
+                if ( this.vars.msgType == 0x10 ) // Entry Assignment
+                {
+                        function addNewEntry()
+                        {
+                                entries[this.vars.entryID] = this.vars;
+                                this.vars = {};
+                        }
+
+                        var kl = 0;
+                        var stack = this.uint16be("keyLength").tap (function() {
+                                this.string("key", this.vars.keyLength)
+                                .uint8("valueType")
+                                .uint16be("entryID")
+                                .uint16be("sequence").tap(function()
+                                {
+                                        if ( this.vars.valueType == 0x00 )
+                                                this.uint8("value").tap(addNewEntry);
+                                        else if ( this.vars.valueType == 0x01 )
+                                                this.doublebe("value").tap(addNewEntry);
+                                });
+                        });
+                }
+                else if ( this.vars.msgType == 0x11 )
+                {
+                        function editExistingEntry()
+                        {
+                                entries[this.vars.entryID].value = this.vars.value;
+                                this.vars = {};
+                        }
+
+                        this.uint16("entryID").
+                        uint16("sequence").tap(function() {
+                                if ( typeof value == "boolean" )
+                                        this.uint8("value").tap(editExistingEntry);
+                                else if ( typeof value == "number" )
+                                        this.doublebe("value").tap(editExistingEntry);
+                        });
+                }
+        });
+});
+
+exports.connect = function(host)
+{
+        sock = net.connect(1735, host, function()
+        {
+                data = Concentrate().uint8(0x01).uint16be(512).result();
+                sock.write(data, "binary");
+                sock.pipe(parser);
+        });
+};
+
+exports.get = function(key)
+{
+        for ( k in entries )
+        {
+                if (entries[k].key == key)
+                        return entries[k].value;
+        }
+        return undefined;
+};
+
+exports.set = function(key, value)
+{
+        var entry = undefined;
+        for ( k in entries )
+        {
+                if (entries[k].key == key)
+                        entry = entries[k];
+        }
+
+        var data = undefined;
+        if ( entry == undefined )
+        {
+                data = Concentrate()
+                        .uint8(0x10)
+                        .uint16be(key.length).string(key)
+                        .uint8(getAssignmentValueType(value))
+                        .uint16be(0xFFFF)
+                        .uint16be(0x01);
+                if ( typeof value == "boolean" )
+                        data.uint8(value?0x01:0x00);
+                else if ( typeof value == "number" )
+                        data.doublebe(value);
+        }
+        else
+        {
+                entry.sequence++;
+                data = Concentrate()
+                        .uint8(0x11)
+                        .uint16be(entry.entryID)
+                        .uint16be(entry.sequence);
+                if ( typeof value == "boolean" )
+                        data.uint8(value?0x01:0x00);
+                else if ( typeof value == "number" )
+                        data.doublebe(value);
+        }
+        d = data.result();
+        sock.write(d, "binary");
+};
+
